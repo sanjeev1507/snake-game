5b396230ce9744f2803522e3f6967807d5000380
sanjeev1507 <117666507+sanjeev1507@users.noreply.github.com>
2023-08-03 13:09:37 +0530

    Add files via upload

--- snake.py
+++ snake.py
@@ -1,36 +1,43 @@
 from turtle import Turtle
-STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
-MOVE_DISTANCE = 20
-UP = 90
-DOWN = 270
-LEFT = 180
-RIGHT = 0
-
-
+STARTING_POSITIONS = [(0, 0), (-20, 0),(-40, 0)]
+MOVE_DISTANCE=20
+UP=90
+DOWN=270
+LEFT=180
+RIGHT=0
 class Snake:
-
     def __init__(self):
-        self.segments = []
+        self.segments=[]
         self.create_snake()
-        self.head = self.segments[0]
+        self.head=self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    #adding new segment to the snake
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
 
     def move(self):
-        for seg_num in range(len(self.segments) - 1, 0, -1):
-            new_x = self.segments[seg_num - 1].xcor()
-            new_y = self.segments[seg_num - 1].ycor()
-            self.segments[seg_num].goto(new_x, new_y)
-        self.head.forward(MOVE_DISTANCE)
+            for seg_num in range(len(self.segments) - 1, 0, -1):
+                new_x = self.segments[seg_num - 1].xcor()
+                new_y = self.segments[seg_num - 1].ycor()
+                self.segments[seg_num].goto(new_x, new_y)
+            self.head .forward(MOVE_DISTANCE)
+
 
     def up(self):
-        if self.head.heading() != DOWN:
+        if self.head.heading()!=DOWN:
             self.head.setheading(UP)
 
     def down(self):
@@ -44,3 +51,5 @@ class Snake:
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
+
+
